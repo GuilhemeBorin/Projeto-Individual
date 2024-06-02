225c113d180249d0f7fd63f8613c4115b4fdb451
GuilhemeBorin <guilherme.galeno@sptech.school>
2024-06-02 11:28:13 -0300

    Formulário semi-finalizado

--- Banco de Dados (BD)/projetoIndividual.sql
+++ Banco de Dados (BD)/projetoIndividual.sql
@@ -1,47 +1,47 @@
-CREATE DATABASE projetoindividual;
-USE projetoindividual;
+-- CREATE DATABASE projetoindividual;
+-- USE projetoindividual;
 
-CREATE TABLE usuario (
-    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45),
-    email VARCHAR(45),
-    senha VARCHAR(45),
-    idade CHAR(5)
-);
+-- CREATE TABLE usuario (
+--     idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+--     nome VARCHAR(45),
+--     email VARCHAR(45),
+--     senha VARCHAR(45),
+--     idade CHAR(5)
+-- );
 
-CREATE TABLE quiz (
-    idQuiz INT PRIMARY KEY AUTO_INCREMENT,
-    fkUsuario INT,
-    qtdPerguntas INT DEFAULT 11, 
-    qtdAcertos INT,
-    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
-);
+-- CREATE TABLE quiz (
+--     idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+--     fkUsuario INT,
+--     qtdPerguntas INT DEFAULT 11, 
+--     qtdAcertos INT,
+--     FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+-- );
 
-create table album (
-idAlbum int primary key auto_increment,
-nomeAlbum varchar(50),
-qtdVotos int
-);
+-- create table album (
+-- idAlbum int primary key auto_increment,
+-- nomeAlbum varchar(50),
+-- qtdVotos int
+-- );
 
-create table formulario (
-idForm int primary key auto_increment,
-fkUsuario int,
-sexoUsuario char(1),
-fkAlbum int,
-foreign key (fkAlbum) references album(idAlbum),
-foreign key (fkUsuario) references usuario(idUsuario)
-);
+-- create table formulario (
+-- idForm int primary key auto_increment,
+-- fkUsuario int,
+-- sexoUsuario char(1),
+-- fkAlbum int,
+-- foreign key (fkAlbum) references album(idAlbum),
+-- foreign key (fkUsuario) references usuario(idUsuario)
+-- );
 
-INSERT INTO album (nomeAlbum) VALUES 
-('O Rappa'),
-('Rappa Mundi'),
-('Lado B Lado A'),
-('O Silêncio Q Precede o Esporro'),
-('7 Vezes'),
-('Nunca Tem Fim...'),
-('Nao Possui');
+-- INSERT INTO album (nomeAlbum) VALUES 
+-- ('O Rappa'),
+-- ('Rappa Mundi'),
+-- ('Lado B Lado A'),
+-- ('O Silêncio Q Precede o Esporro'),
+-- ('7 Vezes'),
+-- ('Nunca Tem Fim...'),
+-- ('Nao Possui');
 
 
-SELECT * FROM usuario;
-SELECT * FROM quiz;
-SELECT * FROM album;
+-- SELECT * FROM usuario;
+-- SELECT * FROM quiz;
+-- SELECT * FROM album;
